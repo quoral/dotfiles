ec6c36da5d14f8294b1b5c99901b27065b227412
Karl Johan Krantz <krantz@spotify.com>
2013-07-13 10:30:23 +0200

    Removed the dot
    Change-Id: I8bb00ea9516b0f8528a01f435b37e1d4ffa03913

---
+++ emacs.d/elpa/puppet-mode-0.2/puppet-mode-autoloads.el
@@ -0,0 +1,32 @@
+;;; puppet-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (puppet-mode) "puppet-mode" "puppet-mode.el" (20946
+;;;;;;  59399 0 0))
+;;; Generated autoloads from puppet-mode.el
+
+(autoload 'puppet-mode "puppet-mode" "\
+Major mode for editing puppet manifests.
+
+The variable puppet-indent-level controls the amount of indentation.
+\\{puppet-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("puppet-mode-pkg.el") (20946 59399 959439
+;;;;;;  0))
+
+;;;***
+
+(provide 'puppet-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; puppet-mode-autoloads.el ends here
