e012fe390c261b9988e4d5cee4c2becea0f6f57b
Karl Johan Krantz <karl-johan.krantz@tink.se>
2020-01-26 22:46:23 +0100

    Add overcomplicated way to lock my screens
    PARALLELISM!!!!!!!

--- files/bin/sway-lock.sh
+++ files/bin/sway-lock.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-LOCKSCREEN_PATH=/tmp/sway-lock.png
-
-grim - | ffmpeg -i pipe: -vf gblur=sigma=5 -y $LOCKSCREEN_PATH
-swaylock --image $LOCKSCREEN_PATH
+LOCKSCREEN_PATH=/tmp/sway-lock
+sway_screens=$(swaymsg -t get_outputs | jq --raw-output  "del(.[] | select(.active == false)) | .[].name")
+echo -e $sway_screens | tr -d '\n' | xargs -d ' ' -I{} -P 4 sh -c "grim -o {} - | ffmpeg -i pipe: -vf gblur=sigma=5 -y $LOCKSCREEN_PATH-{}.png"
+backgrounds=$(echo -e $sway_screens | tr -d '\n' | xargs -d ' ' -I{} -P 4 echo "--image {}:$LOCKSCREEN_PATH-{}.png" | tr '\n' ' ')
+swaylock $backgrounds
