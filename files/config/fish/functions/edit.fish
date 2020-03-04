efbe9bc1be5de8ba7d27aa216f640622b38af719
Karl Johan Krantz <karl-johan.krantz@tink.se>
2020-03-04 09:23:37 +0100

    Sorta wip for edit..

--- files/config/fish/functions/edit.fish
+++ files/config/fish/functions/edit.fish
@@ -1,23 +1,31 @@
 function edit
 
     set search_args ""
-    set EDE code
     getopts $argv | while read -l key option
         switch $key
             case _
-                set search_args (string join \n $search_args $option)
-            case e editor
-                set EDE $option
+                set search_args (string join " " $search_args $option)
         end
     end
-
+    set search_args (string trim -- $search_args)
     if [ "$search_args" ]
         if test -e "$search_args"
-            eval $EDE $search_args
+            open_editor_no_wait $search_args
         else
-            fzf -q "$search_args" | read MYRESULT; and eval $EDE "$MYRESULT"
+            fzf -q "$search_args" | read READRESULT
+            open_editor_no_wait $READRESULT
         end
     else
-        fzf | read MYRESULT; and eval $EDE "$MYRESULT"
+        fzf | read READRESULT
+        open_editor_no_wait $READRESULT
     end
+end
+
+function edit_ag
+     set search_args ""
+     getops
+end
+
+function open_editor_no_wait
+    emacsclient -q $argv &; disown
 end
