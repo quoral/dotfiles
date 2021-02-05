e1a3eff5b992a3780248137ce7b3ad4d54bd1d5c
--global <schwomp@gmail.com>
2021-02-05 09:36:43 +0100

    Doom deserved bazel mode!

--- files/doom.d/config.el
+++ files/doom.d/config.el
@@ -36,6 +36,16 @@
 (customize-set-variable 'org-journal-dir "~/.org/journal/")
 (customize-set-variable 'org-journal-enable-agenda-integration t)
 
+(use-package! bazel-mode
+  :defer t
+  :commands bazel-mode
+  :init
+  (add-to-list 'auto-mode-alist '("BUILD\\(\\.bazel\\)?\\'" . bazel-mode))
+  (add-to-list 'auto-mode-alist '("WORKSPACE\\'" . bazel-mode))
+  :config
+  ;; disable format-all becuase it doesn't sort BUILD list variables
+  (setq bazel-mode-buildifier-before-save t))
+
 (load! "+bindings")
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
