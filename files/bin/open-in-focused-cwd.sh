132f58e89c3903a635757738b332b9edf7a701dd
--global <schwomp@gmail.com>
2021-02-12 17:27:36 +0100

    Fix for folder disappearing underneeth us

--- files/bin/open-in-focused-cwd.sh
+++ files/bin/open-in-focused-cwd.sh
@@ -5,11 +5,14 @@ args=""
 pid=$(swaymsg -t get_tree | jq '.. | select(.type?) | select(.type=="con") | select(.focused==true).pid')
 RESULT=$?
 if [ $RESULT == 0 ]; then
-
     ppid=$(pgrep --newest --parent ${pid})
     cwd=$(readlink /proc/${ppid}/cwd || echo $HOME)
-    if [[ $1 == "kitty" ]]
-    then
+
+    if [ ! -d "$cwd" ]; then
+        cwd="$HOME"
+    fi
+
+    if [[ $1 == "kitty" ]]; then
         args="--directory=${cwd}"
     fi
 fi
